36940d52e1ddfc86ba606829d2611b5fb045abe1
Zain Asgar <zasgar@pixielabs.ai>
2021-05-04 02:19:47 -0700

    remote empty page host

--- gatsby-config.js
+++ gatsby-config.js
@@ -73,13 +73,6 @@ module.exports = {
         ],
       },
     },
-    {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        path: `${__dirname}/content/pages`,
-        name: 'pages',
-      },
-    },
     'gatsby-transformer-sharp',
     {
       resolve: 'gatsby-plugin-sharp',
