f2d5d4281f9f8a2176df950fbc3a74d3bf9d0e50
htroisi <66266496+htroisi@users.noreply.github.com>
2021-05-25 15:23:58 -0700

    Update community.js (#17)

--- src/pages/community.js
+++ src/pages/community.js
@@ -67,27 +67,27 @@ const CommunityPage = () => {
       title: 'Pixienaut Monthly - May',
       date: 'May  27, 2021',
       location: 'Online (Zoom)',
-      link: 'https://calendar.google.com/event?action=TEMPLATE&tmeid=YTVnZ2EybXBxOWZ1bW4zYjNjcnRnampicmpfMjAyMTA1MjdUMTczMDAwWiB6YXNnYXJAcGl4aWVsYWJzLmFp&tmsrc=zasgar%40pixielabs.ai',
+      link: 'https://calendar.google.com/event?action=TEMPLATE&tmeid=YTVnZ2EybXBxOWZ1bW4zYjNjcnRnampicmpfMjAyMTA1MjdUMTczMDAwWiBwaXhpZWxhYnMuYWlfMW84N3ZqdDc4bXI2cWFtOGdhM3BodjFkZTRAZw&tmsrc=pixielabs.ai_1o87vjt78mr6qam8ga3phv1de4%40group.calendar.google.com&scp=ALL',
     }, {
       title: 'Pixienaut Monthly - June',
       date: 'June 24, 2021',
       location: 'Online (Zoom)',
-      link: 'https://calendar.google.com/event?action=TEMPLATE&tmeid=YTVnZ2EybXBxOWZ1bW4zYjNjcnRnampicmpfMjAyMTA2MjRUMTczMDAwWiB6YXNnYXJAcGl4aWVsYWJzLmFp&tmsrc=zasgar%40pixielabs.ai',
+      link: 'https://calendar.google.com/event?action=TEMPLATE&tmeid=YTVnZ2EybXBxOWZ1bW4zYjNjcnRnampicmpfMjAyMTA2MjRUMTczMDAwWiBwaXhpZWxhYnMuYWlfMW84N3ZqdDc4bXI2cWFtOGdhM3BodjFkZTRAZw&tmsrc=pixielabs.ai_1o87vjt78mr6qam8ga3phv1de4%40group.calendar.google.com&scp=ALL',
     }, {
       title: 'Pixienaut Monthly - July',
       date: 'July  22, 2021',
       location: 'Online (Zoom)',
-      link: 'https://calendar.google.com/event?action=TEMPLATE&tmeid=YTVnZ2EybXBxOWZ1bW4zYjNjcnRnampicmpfMjAyMTA3MjJUMTczMDAwWiB6YXNnYXJAcGl4aWVsYWJzLmFp&tmsrc=zasgar%40pixielabs.ai',
+      link: 'https://calendar.google.com/event?action=TEMPLATE&tmeid=YTVnZ2EybXBxOWZ1bW4zYjNjcnRnampicmpfMjAyMTA3MjJUMTczMDAwWiBwaXhpZWxhYnMuYWlfMW84N3ZqdDc4bXI2cWFtOGdhM3BodjFkZTRAZw&tmsrc=pixielabs.ai_1o87vjt78mr6qam8ga3phv1de4%40group.calendar.google.com&scp=ALL',
     }, {
       title: 'Pixienaut Monthly - August',
       date: 'August  26, 2021',
       location: 'Online (Zoom)',
-      link: 'https://calendar.google.com/event?action=TEMPLATE&tmeid=YTVnZ2EybXBxOWZ1bW4zYjNjcnRnampicmpfMjAyMTA4MjZUMTczMDAwWiB6YXNnYXJAcGl4aWVsYWJzLmFp&tmsrc=zasgar%40pixielabs.ai',
+      link: 'https://calendar.google.com/event?action=TEMPLATE&tmeid=YTVnZ2EybXBxOWZ1bW4zYjNjcnRnampicmpfMjAyMTA4MjZUMTczMDAwWiBwaXhpZWxhYnMuYWlfMW84N3ZqdDc4bXI2cWFtOGdhM3BodjFkZTRAZw&tmsrc=pixielabs.ai_1o87vjt78mr6qam8ga3phv1de4%40group.calendar.google.com&scp=ALL',
     }, {
       title: 'Pixienaut Monthly - September',
       date: 'September  23, 2021',
       location: 'Online (Zoom)',
-      link: 'https://calendar.google.com/event?action=TEMPLATE&tmeid=YTVnZ2EybXBxOWZ1bW4zYjNjcnRnampicmpfMjAyMTA5MjNUMTczMDAwWiB6YXNnYXJAcGl4aWVsYWJzLmFp&tmsrc=zasgar%40pixielabs.ai',
+      link: 'https://calendar.google.com/event?action=TEMPLATE&tmeid=YTVnZ2EybXBxOWZ1bW4zYjNjcnRnampicmpfMjAyMTA5MjNUMTczMDAwWiBwaXhpZWxhYnMuYWlfMW84N3ZqdDc4bXI2cWFtOGdhM3BodjFkZTRAZw&tmsrc=pixielabs.ai_1o87vjt78mr6qam8ga3phv1de4%40group.calendar.google.com&scp=ALL',
     },
   ];
   const scrollToSectionId = (section) => {
