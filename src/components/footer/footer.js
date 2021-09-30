b6900bfaee726185ba8eec938bd85b044e41e0ee
Zain Asgar <zasgar@pixielabs.ai>
2021-09-29 22:54:12 -0700

    Add CNCF footer to the website (#35)
    Signed-off-by: Zain Asgar <zasgar@pixielabs.ai>

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -20,31 +20,41 @@ import React from 'react';
 import { Link } from 'gatsby';
 import PropTypes from 'prop-types';
 import styles from './footer.module.scss';
-import pixieLogo from '../../images/pixie-oss.svg';
+import cncfLogo from '../../images/footer/cncf-white.svg';
 import github from '../../images/footer/github-icon.svg';
 
 const Footer = ({ whiteFooter }) => (
   <footer className={`${whiteFooter ? styles.whitefooter : ''} ${styles.footer}`} style={{ position: 'relative' }}>
-    <img src={pixieLogo} alt='Pixie logo' className={styles.logo} />
-    <div className='container hide-not-desktop'>
-      <p align='center'>
-        Pixie was originally created and contributed by
-        {' '}
-        <a href='https://newrelic.com/' target='_blank' rel='noreferrer'>New Relic, Inc.</a>
-      </p>
-    </div>
+    <p align='center'>
+      We are a
+      {' '}
+      <a href='https://cncf.io/' target='_blank' rel='noreferrer'>Cloud Native Computing Foundation</a>
+      {' '}
+      sandbox project.
+    </p>
+    <img src={cncfLogo} alt='CNCF logo' className={styles.logo} />
     <div className={styles.copyrightBar}>
-      <div>
-        <Link to='https://pixielabs.ai/terms' className={styles.mutedLink}>Terms of Service</Link>
+      <div className={styles.verticalCenter}>
+        <Link to='https://www.linuxfoundation.org/terms' className={styles.mutedLink}>Terms of Service</Link>
         <span>
           |
         </span>
-        <Link to='https://pixielabs.ai/privacy' className={styles.mutedLink}>Privacy Policy</Link>
+        <Link to='https://www.linuxfoundation.org/privacy' className={styles.mutedLink}>Privacy Policy</Link>
       </div>
       <div className={styles.centerContent}>
         Copyright © 2018- The Pixie Authors. All Rights Reserved.
+        <br />
+        The Linux Foundation has registered trademarks and uses trademarks.
+        For a list of trademarks of The Linux Foundation, please see our
+        {' '}
+        <a href='https://www.linuxfoundation.org/trademark-usage'>Trademark Usage Page</a>
+        .
+        <br />
+        Pixie was originally created and contributed by
+        {' '}
+        <a href='https://newrelic.com/' target='_blank' rel='noreferrer'>New Relic, Inc.</a>
       </div>
-      <div className={styles.githubLink}>
+      <div className={`${styles.githubLink} ${styles.verticalCenter}`}>
         <a href='https://github.com/pixie-io/px.dev' target='_blank' rel='noreferrer'>
           <img src={github} alt='github' />
           Edit on GitHub
